a8bbf5ff31568aee5f6eaea2d23bb28cfc713ea4
Rıza Berkay AYÇELEBİ <differentiate.function@gmail.com>
2022-05-03 11:28:15 +0300

    tabloya veri eklendi veri okuma işlemi yapıldı

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sqlite
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,31 @@ class MainActivity : AppCompatActivity() {
         try{
 
             val database=openOrCreateDatabase("Products",Context.MODE_PRIVATE,null)
-            database.execSQL("CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY,name VARCHAR,fiyat INT)")
+            database.execSQL("CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY,name VARCHAR,price INT)")
+            //database.execSQL("INSERT INTO products (name,price) VALUES ('Ayakkabi',100)")
+            //database.execSQL("INSERT INTO products (name,price) VALUES ('Elbise',150)")
+            //database.execSQL("INSERT INTO products (name,price) VALUES ('Atkı',50)")
+            //database.execSQL("INSERT INTO products (name,price) VALUES ('Eldiven',20)")
+
+            //val cursor=database.rawQuery("SELECT * FROM products",null)
+
+
+            val cursor=database.rawQuery("SELECT * FROM products WHERE id='5'",null)
+
+            val idColomnIndex=cursor.getColumnIndex("id")
+            val nameColomnIndex=cursor.getColumnIndex("name")
+            val priceColomnIndex=cursor.getColumnIndex("price")
+
+            while(cursor.moveToNext())
+            {
+                textView.text=cursor.getInt(idColomnIndex).toString()
+                textView2.text=cursor.getString(nameColomnIndex).toString()
+                textView3.text=cursor.getInt(priceColomnIndex).toString()
+            }
+
+            cursor.close()
+
+
         }
         catch (e:Exception)
         {
